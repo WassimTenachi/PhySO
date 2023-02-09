74b9b0ef12824d1d91c90bfe5155b52c10c7107c
WassimTenachi <tenachi.wassim@gmail.com>
2023-02-09 20:06:23 +0100

    free const update

--- physr/physym/__init__.py
+++ physr/physym/__init__.py
@@ -1,10 +1,17 @@
-# from . import Token
-# from . import Functions
-# from . import Library
-# from . import Program
-# from . import ExecuteProgram
-# from . import Reward
-# from . import Prior
-# from . import DimensionalAnalysis
-# from . import Dataset
-# from . import Batch
+from . import Token
+from . import Functions
+from . import Library
+from . import Program
+from . import ExecuteProgram
+from . import DimensionalAnalysis
+from . import FreeConstUtils
+from . import Prior
+from . import Reward
+from . import Dataset
+from . import Batch
+
+
+
+
+
+
