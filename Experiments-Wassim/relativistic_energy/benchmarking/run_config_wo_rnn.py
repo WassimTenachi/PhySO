966bafdbe9a274e2c02dc723504ba2aa38635d58
WassimTenachi <tenachi.wassim@gmail.com>
2023-03-09 00:35:27 +0100

    cleaning

--- Experiments-Wassim/relativistic_energy/benchmarking/run_config_wo_rnn.py
+++
@@ -1,8 +0,0 @@
-import physo
-import torch
-import numpy as np
-
-from run_config import *
-
-# Using random number generator instead of RNN
-cell_config.update({"is_lobotomized": True})
--- Experiments-Wassim/relativistic_energy/benchmarking/run_config_wo_units_obs.py
+++
@@ -1,8 +0,0 @@
-import physo
-import torch
-import numpy as np
-
-from run_config import *
-
-# Not observing units
-run_config["learning_config"].update({"observe_units" : False})
