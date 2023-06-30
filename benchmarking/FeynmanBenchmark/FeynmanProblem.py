18ed62dc391d632553f6a85aa07b4bc434e8442f
WassimTenachi <tenachi.wassim@gmail.com>
2023-06-30 14:49:05 +0200

    Feynman problem container class

---
+++ benchmarking/FeynmanBenchmark/FeynmanProblem.py
@@ -0,0 +1,14 @@
+
+import pandas as pd
+import numpy as np
+
+
+class FeynmanProblem:
+    def __int__(self, i_eq):
+        """
+        Loads a Feynman problem.
+        Parameters
+        ----------
+        i_eq : int
+            Equation number in the set of equations.
+        """
