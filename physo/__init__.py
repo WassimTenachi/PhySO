74c5ea3410bf7eb2e0420e60f617fec1ac56f61a
WassimTenachi <tenachi.wassim@gmail.com>
2023-07-03 13:55:02 +0200

    benchmarking submodule

--- physo/__init__.py
+++ physo/__init__.py
@@ -2,6 +2,7 @@ from . import physym
 from . import learn
 from . import task
 from . import config
+from . import benchmark
 
 # Making important interface functions available at root level
 fit = task.fit.fit
