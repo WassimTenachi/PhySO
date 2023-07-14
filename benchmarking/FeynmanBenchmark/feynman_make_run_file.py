a6e9dd9cfcc5977ee2f1c4510f934f457723126d
WassimTenachi <tenachi.wassim@gmail.com>
2023-07-14 17:03:25 +0200

    One jobfile per noize level

--- benchmarking/FeynmanBenchmark/feynman_make_run_file.py
+++ benchmarking/FeynmanBenchmark/feynman_make_run_file.py
@@ -1,7 +1,17 @@
 import numpy as np
 import os
 import shutil
+
+import argparse
 import physo.benchmark.FeynmanDataset.FeynmanProblem as Feyn
+
+parser = argparse.ArgumentParser (description     = "Creates a jobfile to run all Feynman problems.",
+                                  formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("-n", "--noize", default = 0.,
+                    help = "Noize level.")
+config = vars(parser.parse_args())
+
+NOIZE_LEVEL = config["noize"]
 
 # Expected performances on unistra HPC
 # With N_SAMPLES = 1e5 on 1 CPU core -> 40min/10k evaluations
@@ -9,8 +19,6 @@ import physo.benchmark.FeynmanDataset.FeynmanProblem as Feyn
 
 # Nb of trials per problem
 N_TRIALS = 5
-# Noize levels
-NOIZE_LEVELS = [0., 0.1, 0.01, 0.001]
 
 # Output jobfile name
 PATH_OUT_JOBFILE = "jobfile"
@@ -19,21 +27,19 @@ PATH_OUT_JOBFILE = "jobfile"
 EXCLUDED_IN_SRBENCH_EQS_FILENAMES = ['I.26.2', 'I.30.5', 'II.11.17', 'test_10']
 
 commands = []
-# Iterating through noize levels
-for noize_lvl in NOIZE_LEVELS:
-    # Iterating through Feynman problems
-    for i_eq in range (Feyn.N_EQS):
-        print("\nProblem #%i"%(i_eq))
-        # Loading a problem
-        pb = Feyn.FeynmanProblem(i_eq)
-        # Making run file only if it is not in excluded problems
-        if pb.eq_filename not in EXCLUDED_IN_SRBENCH_EQS_FILENAMES:
-            print(pb)
-            # Iterating through trials
-            for i_trial in range (N_TRIALS):
-                # File name
-                command = "python feynman_run.py -i %i -t %i -n %f"%(i_eq, i_trial, noize_lvl)
-                commands.append(command)
+# Iterating through Feynman problems
+for i_eq in range (Feyn.N_EQS):
+    print("\nProblem #%i"%(i_eq))
+    # Loading a problem
+    pb = Feyn.FeynmanProblem(i_eq)
+    # Making run file only if it is not in excluded problems
+    if pb.eq_filename not in EXCLUDED_IN_SRBENCH_EQS_FILENAMES:
+        print(pb)
+        # Iterating through trials
+        for i_trial in range (N_TRIALS):
+            # File name
+            command = "python feynman_run.py -i %i -t %i -n %f"%(i_eq, i_trial, NOIZE_LEVEL)
+            commands.append(command)
 
 # Creating a jobfile containing all commands to run
 jobfile_name    = PATH_OUT_JOBFILE
