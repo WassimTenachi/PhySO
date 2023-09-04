c7ebe1b0102cf5336a6080c60928ee312e1f7275
WassimTenachi <tenachi.wassim@gmail.com>
2023-09-05 01:54:49 +0200

    seed fix, pareto .csv loading demo

--- demos/demos_sr/demo_quick_sr.py
+++ demos/demos_sr/demo_quick_sr.py
@@ -9,15 +9,26 @@
 # External packages
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 # Internal code import
 import physo
 
-# Guard for spawn systems (typically MACs/Windows)
+
+# ### Fixing seed
+
+# In[2]:
+
 if __name__ == '__main__':
+
+    # Seed
+    seed = 0
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
 
     # ### Dataset
 
-    # In[2]:
+    # In[3]:
 
 
     # Dataset
@@ -28,12 +39,12 @@ if __name__ == '__main__':
 
 
     # Where $X=(z,v)$, $z$ being a length of dimension $L^{1}, T^{0}, M^{0}$, v a velocity of dimension $L^{1}, T^{-1}, M^{0}$, $y=E$ if an energy of dimension $L^{2}, T^{-2}, M^{1}$.
-    #
+    # 
     # It be noted that free constants search starts around 1. by default. Therefore when using default hyperparameters, normalizing the data around an order of magnitude of 1 is strongly recommended.
 
     # Dataset plot
 
-    # In[3]:
+    # In[4]:
 
 
     n_dim = X.shape[0]
@@ -52,27 +63,27 @@ if __name__ == '__main__':
 
     # It should be noted that SR capabilities of `physo` are heavily dependent on hyperparameters, it is therefore recommended to tune hyperparameters to your own specific problem for doing science.
     # Summary of available currently configurations:
-    #
+    # 
     # |  Config |                           Notes                           |
     # |:-------:|:---------------------------------------------------------:|
     # | config0 | Light config for demo purposes.                           |
     # | config1 | Tuned on a few physical cases.                            |
     # | config2 | [work in progress] Good starting point for doing science. |
-    #
+    # 
     # By default, `config0` is used, however it is recommended to use the latest configuration currently available (`config1`) as a starting point for doing science.
-    #
+    # 
 
     # #### Running physo
 
     # Given the units input variables $(x_0,..., x_n)$ (here $(z, v)$ ), the root variable $y$ (here $E$) as well as free and fixed constants, you can run an SR task to recover $f$ via:
 
     # (Allowing the use of a fixed constant $1$ of dimension $L^{0}, T^{0}, M^{0}$ (ie dimensionless) and free constants $m$ of dimension $L^{0}, T^{0}, M^{1}$ and $g$ of dimension $L^{1}, T^{-2}, M^{0}$.)
-    #
+    # 
     # It should be noted that here the units vector are of size 3 (eg: `[1, 0, 0]`) as in this example the variables have units dependent on length, time and mass only.
-    # However, units vectors can be of any size $\leq 7$ as long as it is consistent across X, y and constants, allowing the user to express any units (dependent on length, time, mass, temperature, electric current, amount of light, or amount of matter).
+    # However, units vectors can be of any size $\leq 7$ as long as it is consistent across X, y and constants, allowing the user to express any units (dependent on length, time, mass, temperature, electric current, amount of light, or amount of matter). 
     # In addition, dimensional analysis can be performed regardless of the order in which units are given, allowing the user to use any convention ([length, mass, time] or [mass, time, length] etc.) as long as it is consistent across X,y and constants.
 
-    # In[4]:
+    # In[ ]:
 
 
     # Running SR task
@@ -91,7 +102,7 @@ if __name__ == '__main__':
                                 fixed_consts_units = [ [0,0,0] ],
                                 # Free constants names (for display purposes)
                                 free_consts_names = [ "m"       , "g"        ],
-                                # Units of free constants
+                                # Units offFree constants
                                 free_consts_units = [ [0, 0, 1] , [1, -2, 0] ],
                                 # Run config
                                 run_config = physo.config.config0.config0,
@@ -101,7 +112,7 @@ if __name__ == '__main__':
 
     # ### Inspecting the best expression found
 
-    # In[5]:
+    # In[ ]:
 
 
     # Inspecting the best expression found
@@ -118,7 +129,7 @@ if __name__ == '__main__':
 
     # ### Inspecting pareto front expressions
 
-    # In[6]:
+    # In[ ]:
 
 
     # Inspecting pareto front expressions
@@ -133,6 +144,17 @@ if __name__ == '__main__':
         # Showing RMSE
         print("RMSE = {:e}".format(pareto_front_rmse[i]))
         print("-------------\n")
+
+
+    # ### Loading pareto front expressions from log file
+
+    # In[ ]:
+
+
+    # Loading pareto front expressions from .csv log file as sympy expressions
+    sympy_expressions = physo.read_pareto_csv("SR_curves_pareto.csv")
+    for expr in sympy_expressions:
+        print(expr)
 
 
     # In[ ]:
