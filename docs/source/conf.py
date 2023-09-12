d2d492991f2607686b6952975ce0a4f8863ea3e0
WassimTenachi <tenachi.wassim@gmail.com>
2023-09-12 17:44:51 +0200

    using extensions for markdown includes, maths latex dollars, video

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,31 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
+    # For latex style math (works in md too)
+    'sphinx_math_dollar',
+    # Maths
+    'sphinx.ext.mathjax',
+    # Including md files in rst
+    'sphinx_mdinclude',
+    # For markdown
+    # 'myst_parser',  # -> incompatible with sphinx_mdinclude ?
+    # For videos
+    "sphinxcontrib.video",
 ]
+
+mathjax_config = {
+    'tex2jax': {
+        'inlineMath': [ ["\\(","\\)"] ],
+        'displayMath': [["\\[","\\]"] ],
+    },
+}
+
+mathjax3_config = {
+  "tex": {
+    "inlineMath": [['\\(', '\\)']],
+    "displayMath": [["\\[", "\\]"]],
+  }
+}
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
@@ -23,6 +47,11 @@ intersphinx_mapping = {
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # -- Options for HTML output
 
