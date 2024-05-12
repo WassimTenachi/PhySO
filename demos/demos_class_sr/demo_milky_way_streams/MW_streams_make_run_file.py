bfc06a22b38b8d338602900d24bd96fe76af1c49
WassimTenachi <w.tenachi@gmail.com>
2024-05-12 23:30:49 +0200

    Update MW_streams_make_run_file.py

--- demos/demos_class_sr/demo_milky_way_streams/MW_streams_make_run_file.py
+++ demos/demos_class_sr/demo_milky_way_streams/MW_streams_make_run_file.py
@@ -8,7 +8,7 @@ from benchmarking import utils as bu
 PATH_OUT_JOBFILE = "jobfile"
 
 # BENCHMARKING PARAMETERS
-N_TRIALS          = 4
+N_TRIALS          = 10
 NOISE_LEVELS      = [0., 0.001, 0.01, 0.1]
 eps = 1e-6 # -> Will result in only one realization being used
 FRAC_REALIZATIONS = [1., 0.5, 0.25, eps]
