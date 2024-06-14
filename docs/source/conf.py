03b3dca6fc52fc97d1a87e1491083aa1a7478fc8
WassimTenachi <w.tenachi@gmail.com>
2024-06-14 20:39:10 +0200

    custom doc color

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,18 +50,21 @@ intersphinx_mapping = {
 }
 intersphinx_disabled_domains = ['std']
 
-templates_path = ['_templates']
-
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
 }
 
 # -- Options for HTML output
-
 html_theme = 'sphinx_rtd_theme'
 html_logo = '../assets/logo_doc.png'
 
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# -- Customizing style
+html_static_path = ['_static']
+html_css_files = [
+    'css/custom.css',
+]
