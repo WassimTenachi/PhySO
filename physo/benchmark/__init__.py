7bef087e49e6d3d79fcc6a48cd1332feed7be5de
WassimTenachi <w.tenachi@gmail.com>
2025-03-07 15:31:27 -0500

    easier access to Problem classes

--- physo/benchmark/__init__.py
+++ physo/benchmark/__init__.py
@@ -1,3 +1,7 @@
 from . import FeynmanDataset
 from . import ClassDataset
 from . import utils
+
+# Making important interface functions available at root level
+FeynmanProblem = FeynmanDataset.FeynmanProblem.FeynmanProblem
+ClassProblem   = ClassDataset.ClassProblem.ClassProblem
