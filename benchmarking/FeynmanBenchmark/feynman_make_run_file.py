ed284069f792c83b7623377ca0b103e2f41b3ea7
WassimTenachi <w.tenachi@gmail.com>
2025-03-15 19:27:26 -0400

    parallel runs in jobfile

--- benchmarking/FeynmanBenchmark/feynman_make_run_file.py
+++ benchmarking/FeynmanBenchmark/feynman_make_run_file.py
@@ -7,6 +7,12 @@ import physo.benchmark.FeynmanDataset.FeynmanProblem as Feyn
 from benchmarking import utils as bu
 import feynman_configs as fconfigs
 
+# Parallel config :
+# Parallel mode may cause issues due to the number of samples, non-parallel mode is recommended
+# Single core with so many samples will actually use up to 10 cores via pytorch parallelization along sample dim
+PARALLEL_MODE_DEFAULT = False
+N_CPUS_DEFAULT        = 1
+
 # ---------------------------------------------------- SCRIPT ARGS -----------------------------------------------------
 parser = argparse.ArgumentParser (description     = "Creates a jobfile to run all Feynman problems at specified noise level.",
                                   formatter_class = argparse.ArgumentDefaultsHelpFormatter)
@@ -14,12 +20,20 @@ parser.add_argument("-n", "--noise", default = 0.,
                     help = "Noise level.")
 parser.add_argument("-c", "--feynman_config", default = "feynman_config_r10",
                     help = "Feynman config file to use, using feynman_config_r10 by default.")
+parser.add_argument("-p", "--parallel_mode", default = PARALLEL_MODE_DEFAULT,
+                    help = "Should parallel mode be used.")
+parser.add_argument("-ncpus", "--ncpus", default = N_CPUS_DEFAULT,
+                    help = "Nb. of CPUs to use")
 config = vars(parser.parse_args())
 
 # Noise level
 NOISE_LEVEL = float(config["noise"])
 # Feynman config
 FCONFIG = str(config["feynman_config"])
+# Parallel config
+PARALLEL_MODE = int(bool(config["parallel_mode"]))
+N_CPUS        = int(config["ncpus"])
+
 # ---------------------------------------------------- SCRIPT ARGS -----------------------------------------------------
 
 # Expected performances on unistra HPC
@@ -46,7 +60,7 @@ for i_eq in range (Feyn.N_EQS):
         # Iterating through trials
         for i_trial in range (N_TRIALS):
             # File name
-            command = "python feynman_run.py -i %i -t %i -n %f -c %s"%(i_eq, i_trial, NOISE_LEVEL, FCONFIG)
+            command = "python feynman_run.py -i %i -t %i -n %f -c %s -p %i -ncpus %i"%(i_eq, i_trial, NOISE_LEVEL, FCONFIG, PARALLEL_MODE, N_CPUS)
             commands.append(command)
     else:
         print("Problem excluded.")
