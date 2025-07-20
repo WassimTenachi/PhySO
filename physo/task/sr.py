371ef8706e8ef2c21799f1d832b6500d7286c40d
WassimTenachi <w.tenachi@gmail.com>
2025-07-20 15:27:46 -0700

    sr with struct analysis

--- physo/task/sr.py
+++ physo/task/sr.py
@@ -1,14 +1,19 @@
 import numpy as np
 import torch
 import warnings
+import os
+import pandas as pd
 
 # Internal imports
 from physo.config.config0 import config0
+from physo.config.config3_expA import config3
+
 import physo.task.args_handler as args_handler
 import physo
 
 # DEFAULT RUN CONFIG TO USE
-default_config = config0
+default_config          = config0
+default_config_w_struct = config3
 
 def SR(X, y, y_weights=1.,
             # X
@@ -32,6 +37,8 @@ def SR(X, y, y_weights=1.,
             max_n_evaluations = None,
             stop_after_n_epochs = args_handler.default_stop_after_n_epochs,
             epochs = None,
+            # Structure analysis
+            structure_analysis = None,
             # Candidate wrapper
             candidate_wrapper = None,
             # Default run config to use
@@ -104,12 +111,18 @@ def SR(X, y, y_weights=1.,
     epochs : int or None (optional)
         Number of epochs to perform. By default, uses the number in the default config file.
 
+    structure_analysis : bool (optional)
+        If True, a structure analysis of the problem is performed before in order to detect separabilities that can help
+        the symbolic regression task, eg. detecting that problem y = f(x0,x1,x2,x3) can be simplified to
+        y = f(x0) * f(x1) + f(x2,x3). False by default.
+
     candidate_wrapper : callable or None (optional)
         Wrapper to apply to candidate program's output, candidate_wrapper taking func, X as arguments where func is
         a candidate program callable (taking X as arg). By default = None, no wrapper is applied (identity).
     run_config : dict or None (optional)
         Run configuration (by default uses physo.task.sr.default_config)
         See physo/config/ for examples of run configurations.
+        If structure_analysis is True, uses physo.task.sr.default_config_w_struct by default.
     get_run_logger : callable returning physo.learn.monitoring.RunLogger or None (optional)
         Run logger (by default uses physo.task.args_handler.get_default_run_logger)
     get_run_visualiser : callable returning physo.learn.monitoring.RunVisualiser or None (optional)
@@ -129,14 +142,85 @@ def SR(X, y, y_weights=1.,
         Best analytical expression found and run logger.
     """
 
+    # ------- Default run config to use -------
     # Default run config to use
+    using_default_config = False
     if run_config is None:
+        using_default_config = True
         run_config = default_config
     if get_run_logger is None:
         get_run_logger = args_handler.get_default_run_logger
     if get_run_visualiser is None:
         get_run_visualiser = args_handler.get_default_run_visualiser
 
+    # ------- Structure analysis -------
+    # By default, structure analysis is done according to the run_config
+    # - Custom run_config  -> custom structure_analysis
+    # - Default run_config -> no structure_analysis
+    if structure_analysis is None:
+        do_structure_analysis = run_config['struct_analysis']['structure_analysis'] # Consistent with run_config
+    else:
+        do_structure_analysis = bool(structure_analysis) # We will have to check that this is consistent with run_config
+    # If using default config, use default config for structure analysis
+    if do_structure_analysis and using_default_config:
+        run_config = default_config_w_struct # Consistent with run_config
+    # Check if structure_analysis is consistent with run_config, we can not run structure analysis if its hyperparams
+    # are not in run_config
+    assert do_structure_analysis == run_config['struct_analysis']['structure_analysis'], "structure_analysis (%s) is inconsistent with run_config (%s)" %(structure_analysis, run_config['struct_analysis']['structure_analysis'])
+
+    if do_structure_analysis:
+
+        # --- Save path ---
+        if get_run_visualiser().do_save:
+            structure_do_save   = True
+            structure_save_path = ''.join(get_run_visualiser().save_path.split('.')[:-1]) + "_structure_analysis.csv"    # save_path with extension replaced by '_structure_analysis.csv',
+        else:
+            structure_do_save   = False
+            structure_save_path = None
+
+        # --- Check if analysis was already done ---
+        # Check if file structure_save_path does exist, if it does no need to run the analysis
+        do_run = True
+        if structure_do_save:
+            if os.path.exists(structure_save_path):
+                try:
+                    df = pd.read_csv(structure_save_path)
+                    structure        = eval(df["structure_prefix"].iloc[0]) # Loading the structure
+                    structure_pretty = df["structure"].iloc[0]
+                    do_run = False
+                except:
+                    pass
+
+        # --- Run ---
+        if do_run:
+            structure,structure_pretty = physo.StructureAnalysis(
+                        X = X,
+                        y = y,
+                        y_weights = y_weights,
+                        # X
+                        X_names = X_names,
+                        # Candidate wrapper
+                        candidate_wrapper = candidate_wrapper,
+                        # Default run config to use
+                        run_config = run_config,
+                        # Save path
+                        do_save   = structure_do_save,
+                        save_path = structure_save_path,
+                        # Parallel mode
+                        device  = device,
+            )
+        else:
+            print("Structure analysis was already performed, loading it from : %s" %structure_save_path)
+
+        # --- Encoding structure into prior ---
+        print("-> Encoding prior that expressions should be like : %s" %structure_pretty)
+        struct_prior_args = {"structure": structure}
+        struct_prior_args.update(run_config["struct_analysis"]["prior_config"])
+        struct_prior_config = ("StructurePrior", struct_prior_args)
+        run_config["priors_config"].append(struct_prior_config)
+        # todo: adjust global length prior accordingly with the structure ?
+
+    # ------- To Class SR for run -------
     # Transmitting arguments to ClassSR as SR is just a sub-case of ClassSR where there is only one realization
     # and no dataset spe free constants.
     best_expression, run_logger = physo.ClassSR(
