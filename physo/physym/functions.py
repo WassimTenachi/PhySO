23ef5be997954cbe3795d5a94552cb14ce361fce
WassimTenachi <w.tenachi@gmail.com>
2025-07-28 03:06:53 -0400

    fix

--- physo/physym/functions.py
+++ physo/physym/functions.py
@@ -209,7 +209,7 @@ OPS_UNPROTECTED = [
     TokenOp (name = "n4"     , sympy_repr = "n4"     , arity = 1 , complexity = 1 , function = lambda x :torch.pow(x, 4)         ),
 
     # Custom binary operations
-    TokenOp (name = "pow"     , sympy_repr = "pow"   , arity = 2 , complexity = 1 , function = torch_pow                         ),
+    TokenOp (name = "pow"     , sympy_repr = "^"     , arity = 2 , complexity = 1 , function = torch_pow                         ),
 ]
 
 # ------------- protected functions -------------
@@ -296,7 +296,7 @@ OPS_PROTECTED = [
     TokenOp (name = "n4"     , sympy_repr = "n4"     , arity = 1 , complexity = 1 , function = protected_n4     ),
 
     # Custom binary operations
-    TokenOp (name = "pow"     , sympy_repr = "pow"   , arity = 2 , complexity = 1 , function = protected_torch_pow   ),
+    TokenOp (name = "pow"     , sympy_repr = "^"   , arity = 2 , complexity = 1 , function = protected_torch_pow   ),
 
 ]
 
