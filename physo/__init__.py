1faa2c20d7316892c5d5c5dbc51417d3c7d05242
WassimTenachi <w.tenachi@gmail.com>
2025-08-07 23:13:27 -0400

    exposing __version__

--- physo/__init__.py
+++ physo/__init__.py
@@ -8,6 +8,10 @@ from . import benchmark
 fit = task.fit.fit
 SR = task.sr.SR
 ClassSR = task.class_sr.ClassSR
+
 # User level log loading tools
 read_pareto_csv = benchmark.utils.read_logs.read_pareto_csv
 read_pareto_pkl = learn.monitoring.read_pareto_pkl
+
+# Exposed version
+from ._version import __version__
