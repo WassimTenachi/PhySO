7a06e48cbc0cc98780a10bb99932f25617ce34cb
WassimTenachi <w.tenachi@gmail.com>
2025-08-12 21:32:39 -0400

    interfaces

--- physo/toolkit/__init__.py
+++ physo/toolkit/__init__.py
@@ -2,5 +2,7 @@ from . import random_sampler
 from . import codec
 
 # Making important functions accessible:
-get_library               = codec.get_library
+get_library      = codec.get_library
+get_prior        = codec.get_prior
+get_expressions  = codec.get_expressions
 sample_random_expressions = random_sampler.sample_random_expressions
