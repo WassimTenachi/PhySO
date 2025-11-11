062783e5ee843c90d3975b0ecce51eafb4d9f742
WassimTenachi <w.tenachi@gmail.com>
2025-11-10 23:58:16 -0500

    Merge branch 'dev-minor' into science

--- physo/benchmark/utils/metrics_utils.py
+++ physo/benchmark/utils/metrics_utils.py
@@ -15,3 +15,34 @@ def MAE(y_target, y_pred):
 
 def MSE(y_target, y_pred):
     return ((y_target - y_pred) ** 2).mean()
+
+# Pareto front
+
+def get_pareto_front(x, y):
+    """
+    Get indices of Pareto front points.
+    Parameters
+    ----------
+    x : array-like
+        First objective
+    y : array-like
+        Second objective
+    Returns
+    -------
+    pareto_indices : list
+        Indices of Pareto front points
+    """
+    pareto_indices = []
+    for i in range(len(x)):
+        xi, yi = x[i], y[i]
+        # Check if any other point is better in both objectives
+        is_dominated = False
+        for j in range(len(x)):
+            if j != i:
+                xj, yj = x[j], y[j]
+                if (xj <= xi and yj <= yi) and (xj < xi or yj < yi):
+                    is_dominated = True
+                    break
+        if not is_dominated:
+            pareto_indices.append(i)
+    return pareto_indices
